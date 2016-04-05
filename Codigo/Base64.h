b2ee8d386ce626ef70eec3b65e91aac56e0a899f
Francisco Anderson Bezerra Rodrigues <anders1232@mail.com>
2016-04-04 22:40:14 -0300

    Maroto é um arquivo de teste de compressão da base 64. O prog1.c é um simples programa de teste. O prog2.c é um programa para teste de endianess e o prog3.c é o onde fica a main do trabalho em si

--- Codigo/Base64.h
+++ Codigo/Base64.h
@@ -4,6 +4,8 @@
 #ifndef BASE64_H
 #define BASE64_H
 
+#define CODIGO_IGUAL 64
+
 struct elementoBase64
 {
 	uint8_t byte[4];
@@ -32,13 +34,50 @@ char Base64_ObterCaractereCodificado(uint8_t codigo)
 	{
 		return '/';
 	}
+	if(codigo == CODIGO_IGUAL)
+	{
+		return '=';
+	}
 	else
 	{
 		return '?';
 	}
 }
 
-static inline uint32_t Base64_CodificarTexto(ElementoBase64 elemento)
+char Base64_ObterCaractereDecodificado(uint8_t codigo)
+{
+	if(codigo <= 'Z')
+	{
+		return codigo - 'A';
+	}
+	if(codigo <= 'z'  && codigo >= 'a')
+	{
+		return codigo - 71;
+	}
+	if(codigo  >= '0' && codigo <= '9')
+	{
+		return codigo + 4;
+	}
+	if( codigo == '+')
+	{
+		return 62;
+	}
+	if(codigo == '/')
+	{
+		return 63;
+	}
+	if(codigo == '=')
+	{
+		return CODIGO_IGUAL;
+	}
+	else
+	{
+		return '?';
+	}
+}
+
+
+static inline uint32_t Base64_CodificarTexto(ElementoBase64 elemento, uint8_t quantidadeBytesValidos)
 {
 	uint32_t retorno;
 	char* aux= (char *)&retorno;
@@ -46,13 +85,23 @@ static inline uint32_t Base64_CodificarTexto(ElementoBase64 elemento)
 	aux[1]= ( (elemento.byte[0])<<4 | elemento.byte[1]>> 4 ) & 0x3F;
 	aux[2]= ( (elemento.byte[1])<<2 | elemento.byte[2]>> 6 ) & 0x3F;
 	aux[3]= (elemento.byte[2]) & 0x3F;
+	if(quantidadeBytesValidos != 3){
+		if(quantidadeBytesValidos == 1){
+			aux[2]= CODIGO_IGUAL;
+			aux[3]= CODIGO_IGUAL;
+		}
+		if(quantidadeBytesValidos == 2){
+			aux[3]= CODIGO_IGUAL;
+		}
+	}
 	return retorno;
 }
 
-void ImprimirTextoCodificado64(FILE *onde, const char* como, ElementoBase64 oQue)
+void ImprimirTextoCodificado64(FILE *onde, const char* como, ElementoBase64 oQue, int quantBytesValidos)
 {
-	int32_t valorCodificado= Base64_CodificarTexto(oQue);
+	int32_t valorCodificado= Base64_CodificarTexto(oQue, quantBytesValidos);
 	char* aux= (char *)&valorCodificado;
+#ifdef USAR_FPRINTF
 	fprintf(
 		onde,
 		como,
@@ -61,17 +110,37 @@ void ImprimirTextoCodificado64(FILE *onde, const char* como, ElementoBase64 oQue
 		Base64_ObterCaractereCodificado(aux[2]),
 		Base64_ObterCaractereCodificado(aux[3])
 	);
+#else 
+		char codificado[5];
+		sprintf(
+			codificado,
+			"%c%c%c%c",
+	 		Base64_ObterCaractereCodificado(aux[0]),
+			Base64_ObterCaractereCodificado(aux[1]),
+			Base64_ObterCaractereCodificado(aux[2]),
+			Base64_ObterCaractereCodificado(aux[3])
+		);
+		fwrite(codificado, 1, 4, onde);
+#endif
 }
 
-static inline ElementoBase64 Base64_DecodificarTexto(uint32_t texto)
+static inline ElementoBase64 Base64_DecodificarTexto(uint32_t texto, int *bytesValidos/*para retorno por ponteiro*/)
 {
+	*bytesValidos=3;
 	char* aux= (char*)&texto;
+	if(aux[3] == '='){
+		aux[3]=0;
+		(*bytesValidos)--;		
+	}
+	if(aux[2] == '='){
+		aux[2]=0;
+		(*bytesValidos)--;		
+	}
 	ElementoBase64 retorno;
-//	strcpy(retorno.byte, "\0\0\0\0");
-	retorno.byte[0]= (aux[0]<<2)|(aux[1]>>4);
-	retorno.byte[1]= aux[1]<<4 | aux[2]>>2;
-	retorno.byte[2]= aux[2]<<6 | aux[3];
-	retorno.byte[3]= '\0';
+	retorno.byte[0]= Base64_ObterCaractereDecodificado( (aux[0]<<2)|(aux[1]>>4) );
+	retorno.byte[1]= Base64_ObterCaractereDecodificado( aux[1]<<4 | aux[2]>>2 );
+	retorno.byte[2]= Base64_ObterCaractereDecodificado( aux[2]<<6 | aux[3] );
+	retorno.byte[3]= '\0'; 0      4
 	return retorno;
 }
 
---
+++ Codigo/prog3.c
@@ -0,0 +1,102 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdint.h>
+#include<string.h>
+#include"Base64.h"
+
+#define TAM_MAX_NOME_ARQUIVO 150
+
+int main(int argc, char** argv){
+char nomeArq[TAM_MAX_NOME_ARQUIVO+1];
+char codOuDec[4];
+int base;
+	switch(argc){
+		case (1):{
+			printf("Insira o nome do arquivo de entrada:\n");
+			scanf("%s", nomeArq);
+		}
+		case (2):{
+			printf("Queres codificar ou decodificar o arquivo? Digite cod para codificar e dec para decodificar\n");
+			scanf("%s", codOuDec);
+			if(argc ==2){
+				strcpy(nomeArq, argv[1]);
+			}
+		}
+		case (3):{
+			printf("Qual base deseja utilizar para codificar?\n");
+			scanf(" %d", &base);
+			if(argc ==3){
+				strcpy(nomeArq, argv[1]);
+				strcpy(codOuDec, argv[2]);
+			}
+		}
+		case(4):{
+			if(argc == 4){
+				strcpy(nomeArq, argv[1]);
+				strcpy(codOuDec, argv[2]);
+				base = atoi(argv[3]);
+			}
+		}
+	}
+	FILE *arqEntrada= fopen(nomeArq, "rb");
+	char nomeSaida[55];
+	if(!strcmp(codOuDec, "cod")){
+		sprintf(nomeSaida, "%s.b%d", nomeArq, base);
+	}else{
+		snprintf(nomeSaida, strlen(nomeArq)-2, "_%s", nomeArq);
+	}
+	FILE *arqSaida= fopen(nomeSaida, "wb");
+
+	//checando se deu certo a abertura
+
+	if(arqEntrada == NULL){
+		printf("[ERRO] Nao foi possivel abrir o arquivo de entrada.");
+	}
+	if(arqSaida == NULL){
+		printf("[ERRO] Nao foi possivel abrir o arquivo de saida");
+	}
+	ElementoBase64 elemento;
+	int auxAnaliseLeitura;
+	int umPraCod0PraDecod= !strcmp("cod", argv[2]);
+	if(umPraCod0PraDecod){
+		while(!feof(arqEntrada)){
+			auxAnaliseLeitura= fread(elemento.byte, 1, 3, arqEntrada);
+			if(auxAnaliseLeitura != 3){
+				if(auxAnaliseLeitura == 2 ){
+					elemento.byte[2]= 0;
+				}
+				if(auxAnaliseLeitura == 1){
+					elemento.byte[1]= 0;
+					elemento.byte[2]= 0;
+				}
+				if(auxAnaliseLeitura == 0){
+					break;
+				}
+			}
+			ImprimirTextoCodificado64(arqSaida, "%c%c%c%c", elemento, auxAnaliseLeitura);
+		}
+	}else{
+		{
+			int numPraDecodificar;
+			ElementoBase64 elementoDecodificado;
+			int bytesValidos;//para o caso dos = no final do arquivo não se deve imprimir alguns bytes
+			while(!feof(arqEntrada)){
+				fread(&numPraDecodificar, 4, 1, arqEntrada);
+				elementoDecodificado = Base64_DecodificarTexto(numPraDecodificar, &bytesValidos);
+				fprintf(arqSaida, "%c", elementoDecodificado.byte[0]);
+				if(bytesValidos >=2){
+					fprintf(stdout, "%c", elementoDecodificado.byte[1]);
+					fprintf(arqSaida, "%c", elementoDecodificado.byte[1]);
+				}
+				if(bytesValidos ==3){
+					fprintf(stdout, "%c", elementoDecodificado.byte[1]);
+					fprintf(arqSaida, "%c", elementoDecodificado.byte[2]);
+				}
+			}
+		}
+	}
+
+	fclose(arqEntrada);
+	fclose(arqSaida);
+	return 0;
+}
